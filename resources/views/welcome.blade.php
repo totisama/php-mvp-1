7c0ddc8a75c0030a0c1baa548e6d9506956c8aeb
Rodrigo Samayoa <toti1998sama@gmail.com>
2024-11-14 11:06:08 +0100

    display comments

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,12 @@
     <div class='mt-5'>
     <h2 class="font-bold text-lg">{{ $article->title }}</h2>
     <small class="text-gray-400">{{$article->author->name}} | {{$article->published_at->format('d-m-Y')}}</small>
-    <p class="text-sm">{{ $article->summary(100) }}</p>
+    <p class="text-sm">{{ $article->summary(250) }}</p>
+    <ul class="list-disc pl-5">
+      @foreach ($article->comments as $comment)
+      <li>{{ $comment->content }}</li>
+    @endforeach
+    </ul>
     </div>
   @endforeach
 </x-site-layout>
