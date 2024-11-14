a222dd136cad0fbbd2d8b20884941b63bdc87806
Rodrigo Samayoa <toti1998sama@gmail.com>
2024-11-14 11:14:56 +0100

    validation + limit comments

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,10 +2,11 @@
   @foreach ($articles as $article)
     <div class='mt-5'>
     <h2 class="font-bold text-lg">{{ $article->title }}</h2>
-    <small class="text-gray-400">{{$article->author->name}} | {{$article->published_at->format('d-m-Y')}}</small>
+    <small class="text-gray-400">{{$article->author?->name ?? 'Unknown'}} |
+      {{$article->published_at->format('d-m-Y')}}</small>
     <p class="text-sm">{{ $article->summary(250) }}</p>
     <ul class="list-disc pl-5">
-      @foreach ($article->comments as $comment)
+      @foreach ($article->comments->take(3) as $comment)
       <li>{{ $comment->content }}</li>
     @endforeach
     </ul>
