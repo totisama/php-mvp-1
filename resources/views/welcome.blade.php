2c136b27fcf1b4c1f624ee5ef5cdf424e170a359
Rodrigo Samayoa <toti1998sama@gmail.com>
2024-11-15 11:32:29 +0100

    parametrize routes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,22 +1,24 @@
 <x-site-layout title="Welcome page">
-  @foreach ($articles as $article)
-    <a href="/articles/{{$article->id}}" class='mt-5'>
-    <h2 class="font-bold text-lg">{{ $article->title }}</h2>
-    <small class="text-gray-400">
+  <div class="grid grid-cols-2 gap-x-8">
+    @foreach ($articles as $article)
+    <a href="{{route('articles.show', [$article->id])}}" class='mt-5'>
+      <h2 class="font-bold text-lg">{{ $article->title }}</h2>
+      <small class="text-gray-400">
       {{$article->author?->name ?? 'Unknown'}} |
       {{$article->published_at->format('D-M-Y')}}
-    </small>
-    <div>
+      </small>
+      <div>
       @foreach ($article->categories as $category)
       <span class="bg-gray-200 text-gray-800 px-2 py-1 text-xs rounded-full">{{ $category->title }}</span>
     @endforeach
-    </div>
-    <p class="text-sm">{{ $article->summary(250) }}</p>
-    <ul class="list-disc pl-5">
+      </div>
+      <p class="text-sm">{{ $article->summary(250) }}</p>
+      <ul class="list-disc pl-5">
       @foreach ($article->comments->take(3) as $comment)
       <li>{{ $comment->content }}</li>
     @endforeach
-    </ul>
+      </ul>
     </a>
   @endforeach
+  </div>
 </x-site-layout>
