3ca5e6d29a04680e10786b5f56dd2128680d74da
Rodrigo Samayoa <toti1998sama@gmail.com>
2024-11-19 09:11:38 +0100

    Style form and my articles view

--- resources/views/user/articles/edit.blade.php
+++ resources/views/user/articles/edit.blade.php
@@ -1,22 +1,32 @@
 <x-site-layout title="Edit article">
-  <form action="{{route('user.articles.update', $article)}}" method="post">
+
+  <form action="{{route('user.articles.update', $article)}}" method="post" class="w-2/3 border border-gray-300 p-4">
     @method('PUT')
     @csrf
 
-    <div class="flex flex-col">
-      <label for="title">Title</label>
-      <input class="border-2" type="text" name="title" value="{{old('title', $article->title)}}" />
-      @error('title') <span class="text-red-600">{{$message}}</span> @enderror
-    </div>
-    <div class="my-5 flex flex-col">
-      <label for="content">Content</label>
-      <textarea class="border-2" name="content">{{old('content', $article->content)}}</textarea>
-      @error('content') <span class="text-red-600">{{$message}}</span> @enderror
-    </div>
-    <div>
+    <label for="title" class="block text-xs font-semibold uppercase">Title</label>
+    <input name="title" value="{{old('title', $article->title)}}"
+      class="w-2/3 p-1 rounded-lg border border-gray-200 @error('title') border-red-500 @enderror">
+    @error('title')
+  <div class="text-red-500 text-xs">{{$message}}</div> @enderror
 
-      <button type="submit" class="bg-gray-300 py-1 px-2 rounded-xl">Update</button>
-      <a href="{{route('user.articles.index')}}">Back</a>
+    <br /><br />
+
+    <label for="content" class="block text-xs font-semibold uppercase">Content</label>
+    <textarea name="content"
+      class="w-full p-1 rounded-lg border border-gray-200 @error('content') border-red-500 @enderror"
+      rows="5">{{old('content', $article->content)}}</textarea>
+    @error('content')
+  <div class="text-red-500 text-xs">{{$message}}</div> @enderror
+
+    <br /><br />
+
+    <div class="w-full flex justify-end gap-x-8">
+      <a href="{{route('user.articles.index')}}"
+        class="text-xs text-gray-700 bg-gray-300 hover:bg-gray-200 px-4 py-2 rounded uppercase">Undo</a>
+      <button type="submit"
+        class="text-xs text-green-700 bg-green-300 hover:bg-green-200 px-4 py-2 rounded uppercase">Save changes</button>
     </div>
   </form>
+
 </x-site-layout>
--- resources/views/user/articles/index.blade.php
+++ resources/views/user/articles/index.blade.php
@@ -1,25 +1,42 @@
-<x-site-layout title="Articles list">
+<x-site-layout title="Overview of my articles">
+
+  <div class="flex justify-end mb-4">
+    <a class="text-xs text-green-700 bg-green-300 px-1 py-.5 rounded uppercase"
+      href="{{route('user.articles.create')}}">create article</a>
+
+  </div>
+
+
   @if(session()->has('success'))
-    <div class="bg-green-200 text-green-800 p-2 mb-5">{{ session('success') }}</div>
+    <div class="bg-green-100 text-green-500 p-2">
+    {!! session()->get('success') !!}
+    </div>
   @endif
 
-  <a href="{{route('user.articles.create')}}" class="bg-blue-400 px-2 py-1 rounded-xl">Create article</a>
-  <ul class="max-w-4xl flex flex-col gap-5 mt-5">
-    @foreach ($articles as $article)
-    <li>
-      {{$article->title}}
-      <div class="flex">
-      <a href="{{route('user.articles.edit', $article)}}" class="text-sm bg-green-300 px-1 py-2 rounded-xl">
-        Edit
-      </a>
-      <form action="{{route('user.articles.destroy', $article)}}" method="POST">
-        @method('DELETE')
-        @csrf
+  <table class="table-auto w-full border border-gray-300">
+    <thead>
+      <tr class="bg-gray-300 ">
+        <th>Title</th>
+        <th>Actions</th>
+      </tr>
+    </thead>
 
-        <button type="submit" class="text-sm bg-red-300 px-1 py-2 rounded-xl">Delete</button>
-      </form>
-      </div>
-    </li>
+    <tbody>
+      @foreach($articles as $article)
+      <tr class="hover:bg-gray-200 border-b border-gray-200">
+      <td><a href="{{route('user.articles.show', $article)}}">{{$article->title}}</a></td>
+      <td class="flex gap-x-4 justify-center items-center">
+        <a href="{{route('user.articles.edit', $article)}}"
+        class="text-xs text-blue-700 bg-blue-300 px-1 py-.5 rounded uppercase">edit</a>
+        <form action="{{route('user.articles.destroy', $article)}}" method="post">
+        @method('delete')
+        @csrf
+        <button type="submit" class="text-xs text-red-700 bg-red-300 px-1 py-.5 rounded uppercase">delete</button>
+        </form>
+      </td>
+      <tr />
   @endforeach
-  </ul>
+    </tbody>
+  </table>
+
 </x-site-layout>
