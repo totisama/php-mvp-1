993e85bad3f2ccb8ba2862c3545960f060543178
Rodrigo Samayoa <toti1998sama@gmail.com>
2024-11-19 09:16:53 +0100

    add comments and order functions

--- app/Http/Controllers/User/ArticleController.php
+++ app/Http/Controllers/User/ArticleController.php
@@ -15,30 +15,17 @@ class ArticleController extends Controller
 		return view('user.articles.index', compact('articles'));
 	}
 
-	public function edit(string $id)
+	/**
+	 * Show the form for creating a new resource.
+	 */
+	public function create()
 	{
-		$article = Article::find($id);
-
-		return view('user.articles.edit', compact('article'));
+		return view('user.articles.create');
 	}
 
-	public function update(Request $request, string $id)
-	{
-		$request->validate([
-			'title' => ['required', 'string', 'min:5', 'max:255'],
-			'content' => ['required', 'string'],
-		]);
-
-		$article = Article::find($id);
-
-		$article->update([
-			'title' => $request->title,
-			'content' => $request->content,
-		]);
-
-		return redirect()->route('user.articles.index');
-	}
-
+	/**
+	 * Store a newly created resource in storage.
+	 */
 	public function store(Request $request)
 	{
 		$request->validate([
@@ -57,6 +44,9 @@ class ArticleController extends Controller
 		return redirect()->route('user.articles.index');
 	}
 
+	/**
+	 * Display the specified resource.
+	 */
 	public function show(string $id)
 	{
 		$article = Article::findOrFail($id);
@@ -64,12 +54,41 @@ class ArticleController extends Controller
 		return view('articles.show', compact('article'));
 	}
 
-
-	public function create()
+	/**
+	 * Show the form for editing the specified resource.
+	 */
+	public function edit(string $id)
 	{
-		return view('user.articles.create');
+		$article = Article::find($id);
+
+		return view('user.articles.edit', compact('article'));
 	}
 
+	/**
+	 * Update the specified resource in storage.
+	 */
+	public function update(Request $request, string $id)
+	{
+		$request->validate([
+			'title' => ['required', 'string', 'min:5', 'max:255'],
+			'content' => ['required', 'string'],
+		]);
+
+		$article = Article::find($id);
+
+		$article->update([
+			'title' => $request->title,
+			'content' => $request->content,
+		]);
+
+		session()->flash('success', 'Article updated successfully!');
+
+		return redirect()->route('user.articles.index');
+	}
+
+	/**
+	 * Remove the specified resource from storage.
+	 */
 	public function destroy(string $id)
 	{
 		$article = Article::findOrFail($id);
