a677dd8e5203c1e3e5ba2dafdb1e3107f7c326bd
Rodrigo Samayoa <toti1998sama@gmail.com>
2024-11-19 09:58:56 +0100

    create Component

---
+++ app/View/Components/FormText.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\View\Components;
+
+use Closure;
+use Illuminate\Contracts\View\View;
+use Illuminate\View\Component;
+
+class FormText extends Component
+{
+    public function __construct(
+        public string $name,
+        public string $label,
+        public ?string $value = null,
+    ) {
+    }
+
+    public function render(): View|Closure|string
+    {
+        return view('components.form-text', );
+    }
+}
