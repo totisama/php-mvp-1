6411c443a2892ed02bea689e29249c1e6729ba1b
Rodrigo Samayoa <toti1998sama@gmail.com>
2024-11-20 11:20:51 +0100

    format files

--- app/Http/Controllers/User/ArticleController.php
+++ app/Http/Controllers/User/ArticleController.php
@@ -8,125 +8,122 @@ use Illuminate\Http\Request;
 
 class ArticleController extends Controller
 {
-    public function index()
-    {
-        $userId = auth()->user()->id;
+	public function index()
+	{
+		$user = auth()->user();
+		$userId = $user->id;
 
-        if (! $userId) {
-            abort(401);
-        }
+		if (!$userId) {
+			abort(401);
+		}
 
-        $articles = Article::where('author_id', $userId)->get();
+		if ($user->is_admin) {
+			$articles = Article::paginate(5);
+		} else {
+			$articles = Article::where('author_id', $userId)->get();
+		}
 
-        return view('user.articles.index', compact('articles'));
-    }
+		return view('user.articles.index', compact('articles'));
+	}
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        return view('user.articles.create');
-    }
+	/**
+	 * Show the form for creating a new resource.
+	 */
+	public function create()
+	{
+		return view('user.articles.create');
+	}
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        $request->validate([
-            'title' => ['required', 'string', 'min:5', 'max:255'],
-            'content' => ['required', 'string'],
-        ]);
+	/**
+	 * Store a newly created resource in storage.
+	 */
+	public function store(Request $request)
+	{
+		$request->validate([
+			'title' => ['required', 'string', 'min:5', 'max:255'],
+			'content' => ['required', 'string'],
+		]);
 
-        $userId = auth()->user()->id;
+		$userId = auth()->user()->id;
 
-        if (! $userId) {
-            abort(401);
-        }
+		if (!$userId) {
+			abort(401);
+		}
 
-        Article::create([
-            'title' => $request->title,
-            'content' => $request->content,
-            'author_id' => $userId,
-        ]);
+		Article::create([
+			'title' => $request->title,
+			'content' => $request->content,
+			'author_id' => $userId,
+		]);
 
-        session()->flash('success', 'Article created successfully!');
+		session()->flash('success', 'Article created successfully!');
 
-        return redirect()->route('user.articles.index');
-    }
+		return redirect()->route('user.articles.index');
+	}
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        $article = Article::findOrFail($id);
+	/**
+	 * Display the specified resource.
+	 */
+	public function show(string $id)
+	{
+		$article = Article::findOrFail($id);
 
-        $userId = auth()->user()->id;
+		$article->isAuthorized(auth()->user());
 
-        $article->isAuthorized($userId);
+		return view('articles.show', compact('article'));
+	}
 
-        return view('articles.show', compact('article'));
-    }
+	/**
+	 * Show the form for editing the specified resource.
+	 */
+	public function edit(string $id)
+	{
+		$article = Article::find($id);
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        $article = Article::find($id);
+		$article->isAuthorized(auth()->user());
 
-        $userId = auth()->user()->id;
+		return view('user.articles.edit', compact('article'));
+	}
 
-        $article->isAuthorized($userId);
+	/**
+	 * Update the specified resource in storage.
+	 */
+	public function update(Request $request, string $id)
+	{
+		$request->validate([
+			'title' => ['required', 'string', 'min:5', 'max:255'],
+			'content' => ['required', 'string'],
+			'author_id' => ['required', 'numeric'],
+		]);
 
-        return view('user.articles.edit', compact('article'));
-    }
+		$article = Article::find($id);
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        $request->validate([
-            'title' => ['required', 'string', 'min:5', 'max:255'],
-            'content' => ['required', 'string'],
-            'author_id' => ['required', 'numeric'],
-        ]);
+		$article->isAuthorized(auth()->user());
 
-        $article = Article::find($id);
+		$article->update([
+			'title' => $request->title,
+			'content' => $request->content,
+			'author_id' => $request->author_id,
+		]);
 
-        $userId = auth()->user()->id;
+		session()->flash('success', 'Article updated successfully!');
 
-        $article->isAuthorized($userId);
+		return redirect()->route('user.articles.index');
+	}
 
-        $article->update([
-            'title' => $request->title,
-            'content' => $request->content,
-            'author_id' => $request->author_id,
-        ]);
+	/**
+	 * Remove the specified resource from storage.
+	 */
+	public function destroy(string $id)
+	{
+		$article = Article::findOrFail($id);
 
-        session()->flash('success', 'Article updated successfully!');
+		$article->isAuthorized(auth()->user());
 
-        return redirect()->route('user.articles.index');
-    }
+		$article->delete();
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        $article = Article::findOrFail($id);
+		session()->flash('success', 'Article deleted successfully!');
 
-        $userId = auth()->user()->id;
-
-        $article->isAuthorized($userId);
-
-        $article->delete();
-
-        session()->flash('success', 'Article deleted successfully!');
-
-        return redirect()->route('user.articles.index');
-    }
+		return redirect()->route('user.articles.index');
+	}
 }
--- resources/views/user/articles/index.blade.php
+++ resources/views/user/articles/index.blade.php
@@ -8,9 +8,9 @@
 
 
   @if(session()->has('success'))
-  <div class="bg-green-100 text-green-500 p-2">
+    <div class="bg-green-100 text-green-500 p-2">
     {!! session()->get('success') !!}
-  </div>
+    </div>
   @endif
 
   <table class="table-auto w-full border border-gray-300">
@@ -24,24 +24,24 @@
     <tbody>
       @foreach($articles as $article)
       <tr class="hover:bg-gray-200 border-b border-gray-200">
-        <td><a href="{{route('user.articles.show', $article)}}">{{$article->title}}</a></td>
-        <td class="flex gap-x-4 justify-center items-center">
-          @if($article->published_at === null)
-          <a href="{{route('user.articles.publish', $article)}}"
-            class="text-xs text-green-700 bg-green-300 px-1 py-.5 rounded uppercase">publish</a>
-          @else
-          <span class="text-xs text-gray-400">Published</span>
-          @endif
-          <a href="{{route('user.articles.edit', $article)}}"
-            class="text-xs text-blue-700 bg-blue-300 px-1 py-.5 rounded uppercase">edit</a>
-          <form action="{{route('user.articles.destroy', $article)}}" method="post">
-            @method('delete')
-            @csrf
-            <button type="submit" class="text-xs text-red-700 bg-red-300 px-1 py-.5 rounded uppercase">delete</button>
-          </form>
-        </td>
-        <tr />
-        @endforeach
+      <td><a href="{{route('user.articles.show', $article)}}">{{$article->title}}</a></td>
+      <td class="flex gap-x-4 justify-center items-center">
+        @if($article->published_at === null)
+      <a href="{{route('user.articles.publish', $article)}}"
+      class="text-xs text-green-700 bg-green-300 px-1 py-.5 rounded uppercase">publish</a>
+    @else
+    <span class="text-xs text-gray-400">Published</span>
+  @endif
+        <a href="{{route('user.articles.edit', $article)}}"
+        class="text-xs text-blue-700 bg-blue-300 px-1 py-.5 rounded uppercase">edit</a>
+        <form action="{{route('user.articles.destroy', $article)}}" method="post">
+        @method('delete')
+        @csrf
+        <button type="submit" class="text-xs text-red-700 bg-red-300 px-1 py-.5 rounded uppercase">delete</button>
+        </form>
+      </td>
+      <tr />
+  @endforeach
     </tbody>
   </table>
 
